894c03537534fe148eb4b6003064ad75f148d4cd
Jaewook Lim <jaeseung17@me.com>
2023-10-29 12:50:26 +0900

    20231029 3주차 과제 완료

--- Week3/src/main/java/com/test/StartApplication.java
+++ Week3/src/main/java/com/test/StartApplication.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @Controller
 public class StartApplication {
+    @Value("${param.name}")
+    String myName;
+
+    @Value("${param.homework.name}")
+    String homeworkName;
+
+    @Value("${param.homework.due}")
+    String dateString;
 
     @GetMapping("/")
     public String index(final Model model) {
@@ -17,6 +26,18 @@ public class StartApplication {
         return "index";
     }
 
+    @GetMapping("/homework")
+    public String homeworkPage(final Model model) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(homeworkName);
+        sb.append("/");
+        sb.append(dateString);
+        sb.append("까지임.");
+
+        model.addAttribute("title", myName + " Homework");
+        model.addAttribute("msg", sb.toString());
+        return "homework";
+    }
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
